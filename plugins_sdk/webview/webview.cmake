8618ee502456bc11a130113d97c4190639c1b7d0
liucl <66@6666.com>
2025-08-12 17:09:18 +0800

    🔧 build: 修正编译问题

--- plugins_sdk/webview/webview.cmake
+++ plugins_sdk/webview/webview.cmake
@@ -4,7 +4,7 @@ file(GLOB_RECURSE HDRS  "${CMAKE_CURRENT_LIST_DIR}/*.hxx")
 
 add_static(webview_sdk WEBVIEW_EXPORT ${SRC}) 
 setTartetBasic(webview_sdk 
-LIBRARIES ACWebView easy_log
+LIBRARIES ACWebView easy_log iguana
 ) 
 target_compile_definitions(webview_sdk PRIVATE wxDEBUG_LEVEL=0 MODULE_NAME="" ENABLE_STRACE=1)
 set_target_properties(webview_sdk PROPERTIES FOLDER "PluginsSDK")
