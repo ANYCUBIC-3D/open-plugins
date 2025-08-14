2a3fd020709c044ccf2b041a3deda1b20e5c7e73
liucl <66@6666.com>
2025-08-14 14:24:37 +0800

    🐞 fix: 修正release无法链接问题

--- cmake/OpenPluginsTarget.cmake
+++ cmake/OpenPluginsTarget.cmake
@@ -14,17 +14,17 @@ function(create_static_target prefix suffix)
     )
     if(CMAKE_HOST_APPLE)
         set_target_properties(${projectname} PROPERTIES
-            IMPORTED_LOCATION_RELEASE "${ROOT_DIR}/lib/lib${suffix}_s.a"
+            IMPORTED_LOCATION_RELEASE "${ROOT_DIR}/lib/lib${suffix}.a"
             IMPORTED_LOCATION_DEBUG "${ROOT_DIR}/lib/lib${suffix}_s${POSTFIX}.a"
         )
     elseif(CMAKE_HOST_WIN32)
         set_target_properties(${projectname} PROPERTIES
-            IMPORTED_IMPLIB_RELEASE "${ROOT_DIR}/lib/${suffix}_s.lib"
+            IMPORTED_IMPLIB_RELEASE "${ROOT_DIR}/lib/${suffix}.lib"
             IMPORTED_IMPLIB_DEBUG "${ROOT_DIR}/lib/${suffix}_s${POSTFIX}.lib"
         )
     elseif(CMAKE_HOST_LINUX)
         set_target_properties(${projectname} PROPERTIES
-            IMPORTED_LOCATION_RELEASE "${ROOT_DIR}/lib/lib${suffix}_s.a"
+            IMPORTED_LOCATION_RELEASE "${ROOT_DIR}/lib/lib${suffix}.a"
             IMPORTED_LOCATION_DEBUG "${ROOT_DIR}/lib/lib${suffix}_s${POSTFIX}.a"
         )
     endif()
