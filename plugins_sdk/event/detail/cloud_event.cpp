51ade6f1076424e5d25135f41d6b2cf3cf57fea6
liucl <66@6666.com>
2025-08-27 16:38:26 +0800

    ✨ feat(ps): 增加事件定义简化依赖信息

---
+++ plugins_sdk/event/detail/cloud_event.cpp
@@ -0,0 +1,8 @@
+﻿#include "cloud_event.hxx"
+
+#define DEFINE_EXPAND(r, prefix, elem)                                         \
+  wxDEFINE_EVENT(BOOST_PP_CAT(prefix, elem),                                   \
+                 Anycubic::Plugins::SDK::wxPluginEvent);
+
+BOOST_PP_SEQ_FOR_EACH(DEFINE_EXPAND, EVT_CLOUD_CLIENT_,
+                      BOOST_PP_TUPLE_TO_SEQ(CLOUD_CLIENT_EVENT_LIST))
---
+++ plugins_sdk/event/detail/cloud_mqtt_event.hxx
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "plugin_event.hxx"
+
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/seq.hpp>
+#include <boost/preprocessor/tuple.hpp>
+
+#define DECLARE_CLOUD_MQTT_EVENT_LIST                                          \
+  (MULTIBOX_INFO, PERIPHERIE_INFO, DEVICE_STATUS, LAST_WILL_MSG)
+
+#define CLOUD_MQQTT_DECLARE_EXPAND(r, prefix, elem)                            \
+  wxDECLARE_EXPORTED_EVENT(EVENT_API, BOOST_PP_CAT(prefix, elem),              \
+                           Anycubic::Plugins::SDK::wxPluginEvent);
+
+BOOST_PP_SEQ_FOR_EACH(CLOUD_MQQTT_DECLARE_EXPAND, WXEVT_CLOUD_MQTT_,
+                      BOOST_PP_TUPLE_TO_SEQ(DECLARE_CLOUD_MQTT_EVENT_LIST))
