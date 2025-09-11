52bec2d3587a784f44f3aa1a6250c9ad86dca82f
liucl <66@6666.com>
2025-09-11 10:48:41 +0800

    ✨ feat(easy_log): 支持UDP日志输出
    1、接入UDP日志输出方便管理
    2、日志文件名冲突问题解决

--- easy_log/log.cpp
+++ easy_log/log.cpp
@@ -4,13 +4,41 @@
 #include <spdlog/sinks/daily_file_sink.h>
 #include <spdlog/sinks/ostream_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/udp_sink.h>
 #include <spdlog/spdlog.h>
+
+#include <boost/nowide/convert.hpp>
 
 #include <filesystem>
 #include <sstream>
 namespace fs = std::filesystem;
 
 namespace anycubic::logger {
+struct filename_calculator {
+  // Create filename for the form basename.YYYY-MM-DD
+  static std::string calc_filename(const std::string &filename,
+                                   const tm &now_tm) {
+    std::string basename, ext;
+    std::tie(basename, ext) =
+        spdlog::details::file_helper::split_by_extension(filename);
+    std::string count;
+    int nCount = 0;
+    do {
+      auto filename = fmt::format("{}_{:04d}{:02d}{:02d}{}{}", basename,
+                                  now_tm.tm_year + 1900, now_tm.tm_mon + 1,
+                                  now_tm.tm_mday, count, ext);
+#ifdef _WIN32
+      if (!fs::exists(boost::nowide::widen(filename))) {
+#else
+      if (!fs::exists(filename)) {
+#endif
+        return filename;
+      }
+      count = "_" + std::to_string(++nCount);
+    } while (true);
+    return std::string();
+  }
+};
 
 struct BaseConfig {
   bool enable_console{false};
@@ -32,8 +60,7 @@ bool setup_log(const char *path, level_enum default_level,
 }
 
 bool enable_debug(void) {
-  spdlog::apply_all(
-      [](auto logger) { logger->set_level(spdlog::level::trace); });
+  set_level(nullptr, level_enum::trace);
   return true;
 }
 
@@ -45,14 +72,20 @@ bool register_logger(const char *mname, bool) {
       sink->add_sink(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
     }
 
-    sink->add_sink(std::make_shared<spdlog::sinks::daily_file_sink_mt>(
-        (g_config.log_path / (std::string(mname) + ".log")).string(), 0, 0));
+    sink->add_sink(
+        std::make_shared<
+            spdlog::sinks::daily_file_sink<std::mutex, filename_calculator>>(
+            (g_config.log_path / (std::string(mname) + ".log")).string(), 0,
+            0));
 
     auto logger = std::make_shared<spdlog::logger>(mname, sink);
     logger->set_level(
         static_cast<spdlog::level::level_enum>(g_config.default_level));
     logger->set_pattern(DEFAULT_PATTERN);
     spdlog::register_logger(logger);
+    if (strcmp(mname, "MainApp") == 0) {
+      spdlog::set_default_logger(logger);
+    }
     return true;
   } catch (const spdlog::spdlog_ex &) {
     return false;
@@ -70,9 +103,31 @@ bool unregister_logger(const char *mname) {
 
 bool set_level(const char *mname, level_enum level) {
   try {
+      if (mname == nullptr) {
+      spdlog::apply_all([level](std::shared_ptr<spdlog::logger> logger) {
+        logger->set_level(static_cast<spdlog::level::level_enum>(level));
+        if (level <= level_enum::debug) {
+          std::string strHost = "127.0.0.1";
+          if (auto host = std::getenv("AC_LOG_HOST"); host != nullptr) {
+            strHost = host;
+          }
+          spdlog::sinks::udp_sink_config sc{strHost, 55535};
+          auto sink = std::make_shared<spdlog::sinks::udp_sink_mt>(sc);
+          sink->set_pattern(DEFAULT_PATTERN);
+          auto sink0 = logger->sinks();
+          if (auto p = dynamic_cast<dist_sink_mt *>(sink0[0].get());
+              p != nullptr)
+            p->add_sink(sink);
+        }
+      });
+      return true;
+    }
     auto logger = spdlog::get(mname);
     if (logger) {
       logger->set_level(static_cast<spdlog::level::level_enum>(level));
+      if (level <= level_enum::debug) {
+        logger->flush_on(static_cast<spdlog::level::level_enum>(level));
+      }
       return true;
     }
   } catch (const spdlog::spdlog_ex &) {
