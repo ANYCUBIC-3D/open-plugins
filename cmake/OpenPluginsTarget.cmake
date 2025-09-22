31b6a38dd508c1a5a46026fb3f6f1ce021cccc55
liucl <6666@66.com>
2025-09-22 14:50:46 +0800

    🐞 fix: 修正文件查找处理错误

--- cmake/OpenPluginsTarget.cmake
+++ cmake/OpenPluginsTarget.cmake
@@ -22,7 +22,7 @@ function(create_static_target prefix suffix)
         )
     elseif(CMAKE_HOST_WIN32)
         set_target_properties(${projectname} PROPERTIES
-            IMPORTED_IMPLIB_RELEASE "${ROOT_DIR}lib/${suffix}.lib"
+            IMPORTED_IMPLIB_RELEASE "${ROOT_DIR}lib/${suffix}_s.lib"
             IMPORTED_IMPLIB_DEBUG "${ROOT_DIR}lib/${suffix}_s${POSTFIX}.lib"
         )
     elseif(CMAKE_HOST_LINUX)
