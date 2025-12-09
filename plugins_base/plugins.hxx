db88a352b0f681daed7f2430a47f6baf74b0af13
liucl <66@6666.com>
2025-12-09 17:39:27 +0800

    🦄 refactor: 更新接口定义

--- plugins_base/plugins.hxx
+++ plugins_base/plugins.hxx
@@ -103,14 +103,14 @@ struct Plugin {
 
   /**
    * @brief 创建插件对话框
-   * @param dlg 对话框对象
+   * @param panel 面板对象
    * @param parent 父窗口指针
    * @param bmp 位图资源指针
    * @return true 创建成功
    * @return false 创建失败
    */
-  virtual bool CreateDialog(wxDialog *dlg, wxWindow *parent = nullptr,
-                            wxString *bmp = nullptr) = 0;
+  virtual bool CreatePanel(wxPanel *panel, wxWindow *parent = nullptr,
+                           wxString *bmp = nullptr) = 0;
   /**
    * @brief 创建插件Webview
    *
