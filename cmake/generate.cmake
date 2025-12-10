3d2bae525be9e075a6168424e1f621e197a5c0e3
liucl <66@6666.com>
2025-12-10 15:12:22 +0800

    feat: i18n基础支持

--- cmake/generate.cmake
+++ cmake/generate.cmake
@@ -58,3 +58,145 @@ function(generate_project include_dir work_dir)
         module_install(share/${PROJECT_NAME} "${CMAKE_CURRENT_BINARY_DIR}/GenerateTarget.cmake")
     endif()
 endfunction()
+
+option(ENABLE_I18N "Enable i18n support" ON)
+
+if(ENABLE_I18N)
+    find_package(Gettext REQUIRED)
+    cmake_path(GET GETTEXT_MSGFMT_EXECUTABLE PARENT_PATH CURRENT_PATH)
+    find_program(GETTEXT_XGETTEXT_EXECUTABLE xgettext PATHS ${CURRENT_PATH} REQUIRED NO_CACHE NO_DEFAULT_PATH)
+endif()
+
+
+function(i18n_generate_pot target pot destdir)
+    if(NOT ENABLE_I18N)
+        return()
+    endif()
+    if(NOT TARGET ${target})
+        message(FATAL_ERROR "Target ${target} does not exist")
+    endif()
+    get_target_property(target_source_files ${target} SOURCES)
+    get_target_property(target_name ${target} NAME)
+    get_target_property(target_bin_dir ${target} BINARY_DIR)
+
+    set(listfile ${target_bin_dir}/${target_name}.txt)
+    file(WRITE ${listfile} "")
+    foreach(source_file ${target_source_files})
+        file(APPEND ${listfile} "${source_file}\n")
+    endforeach()
+    # 这里是需要直接生成pot文件
+    execute_process(COMMAND ${GETTEXT_XGETTEXT_EXECUTABLE} --package-name=${target_name} --from-code=UTF-8 --keyword=L --keyword=_ --keyword=_L --keyword=_u8L --keyword=L_CONTEXT:1,2c --keyword=_L_PLURAL:1,2 --add-comments=TRN -n --debug --boost -f ${listfile} -o ${pot}
+        WORKING_DIRECTORY ${target_bin_dir} ERROR_QUIET
+        COMMAND_ECHO STDOUT ECHO_ERROR_VARIABLE ECHO_OUTPUT_VARIABLE)
+    if(NOT EXISTS ${pot})
+        message(WARNING "target ${target_name} pot file does not exist")
+        return()
+    endif()
+    # 合并po信息
+    if(NOT destdir MATCHES "/$")
+        set(destdir "${destdir}/")
+    endif()
+    foreach(lan ${ARGN})
+        if(NOT EXISTS ${destdir}${lan})
+            file(MAKE_DIRECTORY ${destdir}${lan})
+        endif()
+        set(_poFile ${destdir}${lan}/${target_name}.po)
+        if(EXISTS ${_poFile})
+            execute_process(COMMAND ${GETTEXT_MSGMERGE_EXECUTABLE} -q -U -N ${_poFile} ${pot}
+                WORKING_DIRECTORY ${destdir}${lan} ERROR_QUIET
+                COMMAND_ECHO STDOUT ECHO_ERROR_VARIABLE ECHO_OUTPUT_VARIABLE)
+        else()
+            # Read the generated .po file
+            file(READ ${pot} POT_CONTENT)
+            # Replace CHARSET with UTF-8
+            string(REPLACE "Content-Type: text/plain; charset=CHARSET" "Content-Type: text/plain; charset=UTF-8" POT_CONTENT_UTF8 "${POT_CONTENT}")
+            # Write the updated content back to the file
+            file(WRITE ${_poFile} "${POT_CONTENT_UTF8}")
+        endif()
+    endforeach()
+endfunction()
+
+function(i18n_generate_mo search_dir)
+    if(NOT ENABLE_I18N)
+        return()
+    endif()
+    file(GLOB_RECURSE po_files ${search_dir}/*.po)
+    foreach(po_file ${po_files})
+        cmake_path(GET po_file PARENT_PATH CURRENT_PATH)
+        cmake_path(GET po_file STEM po_filename)
+        cmake_path(GET CURRENT_PATH STEM LANGUAGE_NAME)
+        set(mo_file ${search_dir}${LANGUAGE_NAME}/${po_filename}.mo)
+        
+        execute_process(COMMAND ${GETTEXT_MSGFMT_EXECUTABLE} --check-format -o ${mo_file} ${po_file}
+            WORKING_DIRECTORY ${CURRENT_PATH} ERROR_QUIET
+            COMMAND_ECHO STDOUT ECHO_ERROR_VARIABLE ECHO_OUTPUT_VARIABLE)
+    endforeach()
+endfunction()
+
+function(generate_mo_source_file SRC PACKAGE TEMPLATE)
+    file(MD5 ${PACKAGE} PACKAGE_MD5)
+    file(SIZE ${PACKAGE} PACKAGE_SIZE)
+     # 判断文件大小是否为 0，若为 0 则终止 CMake 配置
+     if (PACKAGE_SIZE EQUAL 0)
+        message(FATAL_ERROR "file ${PACKAGE} is empty")
+    endif()
+    # read hex data from file
+    file(READ ${PACKAGE} PACKAGE_DATA HEX)
+    # convert hex data for C compatibility
+    string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," PACKAGE_DATA "${PACKAGE_DATA}")
+
+    configure_file(${TEMPLATE} ${SRC} @ONLY)
+endfunction()
+
+
+macro(i18n_generate_embed target search_dir)
+    if(NOT ENABLE_I18N)
+        return()
+    endif()
+    if(NOT TARGET ${target})
+        message(FATAL_ERROR "Target ${target} does not exist")
+    endif()
+    get_target_property(target_bin_dir ${target} BINARY_DIR)
+    set(template ${target_bin_dir}/mo_template.cpp.in)
+    set(header ${target_bin_dir}/${target}_languages.hpp)
+    file(WRITE ${template}
+        "#include <stdint.h>\n"
+        "#include <tuple>\n"
+        "using result_type = std::tuple<uint32_t, const uint8_t *, const char *>;\n"
+        "// clang-format off\n"
+        "static const char* PACKAGE_MD5 = \"@PACKAGE_MD5@\";\n"
+        "static constexpr uint32_t PACKAGE_SIZE = @PACKAGE_SIZE@;\n"
+        "static const uint8_t PACKAGE_DATA[] = {@PACKAGE_DATA@};\n"
+        "result_type @LANG@() {\n"
+        "return std::make_tuple(PACKAGE_SIZE, PACKAGE_DATA, PACKAGE_MD5);\n"
+        "}\n"
+        "// clang-format on\n"
+        "\n"
+        )
+    file(WRITE ${header}
+        "#include <stdint.h>\n"
+        "#include <tuple>\n"
+        "using result_type = std::tuple<uint32_t, const uint8_t *, const char *>;\n"
+        "#define LANGUAGE_LIST ("
+        )
+    file(GLOB_RECURSE mo_files ${search_dir}/*.mo)
+    
+    foreach(mo_file ${mo_files})
+        cmake_path(GET mo_file STEM mo_filename)
+        cmake_path(GET mo_file PARENT_PATH CURRENT_PATH)
+        cmake_path(GET CURRENT_PATH STEM LANG)
+        file(APPEND ${header}
+            "${LANG},"
+            )
+        set(embed_file ${target_bin_dir}/${mo_filename}_${LANG}.cpp)
+        list(APPEND target_source_files ${embed_file})
+        generate_mo_source_file(${embed_file} ${mo_file} ${template})
+    endforeach()
+    file(APPEND ${header}
+        "padding)\n"
+        )
+    set_source_files_properties(${target_source_files} PROPERTIES GENERATED TRUE)
+    target_sources(${target} PRIVATE ${target_source_files})
+    target_include_directories(${target} PRIVATE ${target_bin_dir})
+endmacro()
+
---
+++ plugins_base/i18n_helper.hxx
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <boost/preprocessor/if_comma.hpp>
+#include <boost/preprocessor/repeat.hpp>
+#include <boost/preprocessor/seq.hpp>
+#include <boost/preprocessor/stringize.hpp>
+#include <boost/preprocessor/tuple.hpp>
+
+#define I18N_DECLARE_LANG(r, nil, lang) extern result_type lang();
+
+#define I18N_DECLARE_LANGUAGES(lang_list)                                      \
+  BOOST_PP_SEQ_FOR_EACH(                                                       \
+      I18N_DECLARE_LANG, _,                                                    \
+      BOOST_PP_SEQ_POP_BACK(BOOST_PP_TUPLE_TO_SEQ(lang_list)))
+
+#define I18N_DEFINE_LANG(r, n, lang_list)                                      \
+  BOOST_PP_IF_COMMA(n) {                                                       \
+    BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(n, lang_list)),                      \
+                       BOOST_PP_TUPLE_ELEM(n, lang_list)                       \
+  }
+
+#define I18N_DEFINE_LANGUAGES(type, value, lang_list)                          \
+  type value = {BOOST_PP_REPEAT(BOOST_PP_TUPLE_SIZE(lang_list) - 1,            \
+                                I18N_DEFINE_LANG, lang_list)};
