3b282ba9c53687610fa2af1b60409d2c38f216c3
liucl <66@6666.com>
2025-12-15 19:12:27 +0800

    fix: 编译错误

--- utility/codec/base64.hxx
+++ utility/codec/base64.hxx
@@ -8,4 +8,11 @@
  * @return std::string 输出base64编码字符串
  */
 std::string base64Encode(const std::string &src);
-std::string base64Dec
+
+/**
+ * @brief base64 标准解码实现
+ *
+ * @param src base64编码字符串
+ * @return std::string 输出解码后的字符串
+ */
+std::string base64Decode(const std::string &src);
