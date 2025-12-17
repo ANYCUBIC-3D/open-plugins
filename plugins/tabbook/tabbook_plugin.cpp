ec12d9c19dcd31b03fe9c402f70db5e3f5b48933
liucl <66@6666.com>
2025-12-17 10:49:30 +0800

    feat: 更新测试项目

--- plugins/tabbook/tabbook_plugin.cpp
+++ plugins/tabbook/tabbook_plugin.cpp
@@ -39,8 +39,7 @@ bool TabBookPlugin::AttachEvt(wxEvtHandler *) { return false; }
 
 bool TabBookPlugin::DetachEvt(wxEvtHandler *) { return false; }
 
-bool TabBookPlugin::CreatePanel(wxPanel *panel, wxWindow *parent,
-                                wxString *bmp) {
+bool TabBookPlugin::BindEvt(wxPanel *panel, wxWindow *parent, wxString *bmp) {
   return false;
 }
 
