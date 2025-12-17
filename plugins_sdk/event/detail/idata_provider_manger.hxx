4ca2e88375bf991d3d9e5673a367a09c4aced93a
wangliang <wangliang@anycubic.com>
2025-12-17 10:31:57 +0800

    接口文件

---
+++ plugins_sdk/event/detail/idata_provider_manger.hxx
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <wx/wx.h>
+#include "custom_struct_manger.hxx"
+#include "plugin_custom_event.hxx"
+
+class IDataProvider_AmsButton
+{
+public:
+    virtual ~IDataProvider_AmsButton() {}
+
+    virtual wxVector<AmsBoxObj>     GetPrinterKeyAmsInfoMap(wxString deviceID) const   = 0;
+    virtual void                    SendFilamentSlotChangeEvent() const = 0;
+    virtual wxVector<PrinterObj>    GetPrinterList(wxString selectPrinterType) const   = 0;
+    virtual void BroadcastCloudEvent(wxPluginEvent* event) const = 0;
+
+};
+
+
+class IDataProvider_printerImage
+{
+public:
+    virtual ~IDataProvider_printerImage() {}
+
+    virtual void ReginsterCloudEvent(wxWindow* win) const = 0;
+    virtual void UnreginsterCloudEvent(wxWindow* win) const = 0;
+};
+
+
+class IDataProvider_AsmDialog
+{
+public:
+    virtual ~IDataProvider_AsmDialog() {}
+
+    virtual wxVector<AmsBoxObj>  GetPrinterKeyAmsInfoMap(wxString deviceID) const = 0;
+};
+
+
+class IDataProvider_colorPanel
+{
+public:
+    virtual ~IDataProvider_colorPanel() {}
+
+    virtual void ReginsterCloudEvent(wxWindow* win) const   = 0;
+    virtual void UnreginsterCloudEvent(wxWindow* win) const = 0;
+};
