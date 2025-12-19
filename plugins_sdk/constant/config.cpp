c233bf6f995e6c60b2ffdf1ace5a50d8764ed280
wangliang <wangliang@anycubic.com>
2025-12-19 11:26:53 +0800

    账号

--- plugins_sdk/constant/config.cpp
+++ plugins_sdk/constant/config.cpp
@@ -2,3 +2,6 @@
 
 const char *CONFIG_DOWNLOAD_PATH = "download_path";
 const char *CONFIG_ACCESS_TOKEN_KEY = "access_token";
+
+const char* CONFIG_CLOUD_ACCCOUNT = "anycubic_cloud.account";
+const char* CONFIG_CLOUD_PASSWORD = "anycubic_cloud.password";
--- plugins_sdk/constant/config.hxx
+++ plugins_sdk/constant/config.hxx
@@ -2,4 +2,6 @@
 extern "C" {
 extern const char *CONFIG_DOWNLOAD_PATH;
 extern const char *CONFIG_ACCESS_TOKEN_KEY;
+extern const char* CONFIG_CLOUD_ACCCOUNT;
+extern const char* CONFIG_CLOUD_PASSWORD;
 }
