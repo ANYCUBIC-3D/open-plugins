f45c136ad9920f0fbb4a586dea1c3d3823978d16
liucl <66@6666.com>
2026-01-08 15:28:07 +0800

    🌈 style: 清理未使用定义

--- plugins_sdk/constant/config.cpp
+++ plugins_sdk/constant/config.cpp
@@ -1,7 +1,3 @@
 ﻿#include "config.hxx"
 
 const char *CONFIG_DOWNLOAD_PATH = "download_path";
-const char *CONFIG_ACCESS_TOKEN_KEY = "access_token";
-
-const char* CONFIG_CLOUD_ACCCOUNT = "anycubic_cloud.account";
-const char* CONFIG_CLOUD_PASSWORD = "anycubic_cloud.password";
--- plugins_sdk/constant/config.hxx
+++ plugins_sdk/constant/config.hxx
@@ -1,7 +1,4 @@
 ﻿#pragma once
 extern "C" {
 extern const char *CONFIG_DOWNLOAD_PATH;
-extern const char *CONFIG_ACCESS_TOKEN_KEY;
-extern const char* CONFIG_CLOUD_ACCCOUNT;
-extern const char* CONFIG_CLOUD_PASSWORD;
 }
