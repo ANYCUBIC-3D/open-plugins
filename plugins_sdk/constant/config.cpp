db7123b4e7c4f0d93f89e80489f555fbaa28a86e
liucl <66@6666.com>
2026-01-15 16:38:39 +0800

    feat: 增加配置名

--- plugins_sdk/constant/config.cpp
+++ plugins_sdk/constant/config.cpp
@@ -1,3 +1,5 @@
 ﻿#include "config.hxx"
 
 const char *CONFIG_DOWNLOAD_PATH = "download_path";
+const char *CONFIG_PLUGINS_PATH = "plugins_dir";
+const char *CONFIG_PACKAGE_PATH = "cache_dir";
--- plugins_sdk/constant/config.hxx
+++ plugins_sdk/constant/config.hxx
@@ -1,4 +1,6 @@
 ﻿#pragma once
 extern "C" {
 extern const char *CONFIG_DOWNLOAD_PATH;
+extern const char *CONFIG_PLUGINS_PATH;
+extern const char *CONFIG_PACKAGE_PATH;
 }
